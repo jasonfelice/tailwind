a0550d266fa1977f4e03957c50611126707a563a
Jake Felice <jasonfelice0@gmail.com>
2024-08-01 03:39:13 -0700

    Change logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App font-Poppins">
       <header>
         <nav className="container flex items-center py-4 mt-4 sm:mt-12">
-          <div className="py-1 flex items-center gap-3 text-xl"><img width={40} src="https://www.svgrepo.com/show/6972/pointer-advice.svg" alt="Logo" /> Lokate</div>
+          <div className="py-1 flex items-center gap-3 text-xl"><img width={40} src="https://www.svgrepo.com/show/513321/map.svg" alt="Logo" /> Lokate</div>
           <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-app-blue uppercase text-xs">
             <li className="cursor-pointer">Features</li>
             <li className="cursor-pointer">Pricing</li>
