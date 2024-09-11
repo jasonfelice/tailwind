0d05b0ab85ba9f8acb2affc1c4d306c2335df78e
Jake Felice <jasonfelice0@gmail.com>
2024-09-11 05:11:00 -0700

    Fix typo on class name

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
               </button>
             </div>
           </div>
-            <div className="flex justify-cneter flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
+            <div className="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
               <img className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full" src={hero} alt="Hero" />
             </div>
         </div>
